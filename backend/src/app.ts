1ca7c44cd9338419670275bc5212d83c7de4d132
Ashish Sharma <90973051+Sharma4ashish@users.noreply.github.com>
2026-05-14 22:44:11 +0530

    updates

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,4 @@
-import express from "express"
+import express, { Request, Response } from "express"
 import helmet from "helmet";
 import cors from "cors"
 import cookieParser from "cookie-parser"
@@ -6,9 +6,12 @@ import { env } from "./config/env.config.js";
 import logger from "./config/logger.config.js";
 import { globalErrorHandler } from "./middlewares/error.middleware.js";
 import { AppError } from "./utils/AppError.js";
+import pinoHttp from "pino-http";
+
 
 const app = express();
 
+    // app.use(pinoHttp())
 app.use(helmet())
 app.use(cors({
     origin: env.FRONTEND_URL,
@@ -21,14 +24,14 @@ app.use(express.urlencoded({
 }))
 app.use(cookieParser())
 
-app.get("/health-check",(_, res) =>{
+app.get("/health-check",(_, res:Response) =>{
     return res.status(200).json({
         success:true,
         message:"Server is Healthy "
-    });    
+    });
 })
 
-app.use((req, res, next) => {
+app.use((req:Request, res:Response, next) => {
     next(new AppError(`Cannot ${req.method} ${req.originalUrl}`,404))
 })
 
